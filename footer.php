be6be4dffc0c79972961e867c8e099452787503c
tetrabirk <leyensl@isl-edu.be>
2017-10-12 21:22:52 +0200

    debut du decoupage dulayout

--- footer.php
+++ footer.php
@@ -1,2 +1,145 @@
-<?php
-wp_footer();
+
+<!-- Footer -->
+<footer id="footer" class="footer bg-black-222">
+    <div class="container pb-50">
+        <div class="row border-bottom-black">
+            <div class="col-sm-6 col-md-3">
+                <div class="widget dark">
+                    <img class="mt-10 mb-20" alt="" src="images/logo-wide-white.png">
+                    <p>203, Envato Labs, Behind Alis Steet, Melbourne, Australia.</p>
+                    <ul class="list-inline mt-5">
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored mr-5"></i> <a class="text-gray" href="#">123-456-789</a> </li>
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-colored mr-5"></i> <a class="text-gray" href="#">contact@yourdomain.com</a> </li>
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored mr-5"></i> <a class="text-gray" href="#">www.yourdomain.com</a> </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="widget dark">
+                    <h5 class="widget-title line-bottom">Latest News</h5>
+                    <div class="latest-posts">
+                        <article class="post media-post clearfix pb-0 mb-10">
+                            <a href="#" class="post-thumb"><img alt="" src="http://placehold.it/80x55"></a>
+                            <div class="post-right">
+                                <h5 class="post-title mt-0 mb-5"><a href="#">Sustainable Construction</a></h5>
+                                <p class="post-date mb-0 font-12">Mar 08, 2015</p>
+                            </div>
+                        </article>
+                        <article class="post media-post clearfix pb-0 mb-10">
+                            <a href="#" class="post-thumb"><img alt="" src="http://placehold.it/80x55"></a>
+                            <div class="post-right">
+                                <h5 class="post-title mt-0 mb-5"><a href="#">Industrial Coatings</a></h5>
+                                <p class="post-date mb-0 font-12">Mar 08, 2015</p>
+                            </div>
+                        </article>
+                        <article class="post media-post clearfix pb-0 mb-10">
+                            <a href="#" class="post-thumb"><img alt="" src="http://placehold.it/80x55"></a>
+                            <div class="post-right">
+                                <h5 class="post-title mt-0 mb-5"><a href="#">Storefront Installations</a></h5>
+                                <p class="post-date mb-0 font-12">Mar 08, 2015</p>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="widget dark">
+                    <h5 class="widget-title line-bottom">Useful Links</h5>
+                    <ul class="list angle-double-right list-border">
+                        <li><a href="#">Body Building</a></li>
+                        <li><a href="#">Fitness Classes</a></li>
+                        <li><a href="#">Your Service Title</a></li>
+                        <li><a href="#">Yoga Courses</a></li>
+                        <li><a href="#">Training</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="widget dark">
+                    <h5 class="widget-title line-bottom">Opening Hours</h5>
+                    <div class="opening-hours">
+                        <ul class="list-border">
+                            <li class="clearfix"> <span> Mon - Tues :  </span>
+                                <div class="value pull-right"> 6.00 am - 10.00 pm </div>
+                            </li>
+                            <li class="clearfix"> <span> Wednes - Thurs :</span>
+                                <div class="value pull-right"> 8.00 am - 6.00 pm </div>
+                            </li>
+                            <li class="clearfix"> <span> Fri : </span>
+                                <div class="value pull-right"> 3.00 pm - 8.00 pm </div>
+                            </li>
+                            <li class="clearfix"> <span> Sun : </span>
+                                <div class="value pull-right"> Closed </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-10">
+            <div class="col-md-5">
+                <h4 class="text-white">Subscribe Us</h4>
+                <form id="mailchimp-subscription-form-footer" class="newsletter-form mt-20">
+                    <div class="input-group">
+                        <input type="email" value="" name="EMAIL" placeholder="Your Email" class="form-control input-lg font-16" data-height="45px" id="mce-EMAIL-footer" style="height: 45px;">
+                        <span class="input-group-btn">
+                <button data-height="45px" class="btn btn-colored btn-theme-colored btn-xs m-0 font-14" type="submit">Subscribe</button>
+              </span>
+                    </div>
+                </form>
+                <!-- Mailchimp Subscription Form Validation-->
+                <script type="text/javascript">
+                    $('#mailchimp-subscription-form-footer').ajaxChimp({
+                        callback: mailChimpCallBack,
+                        url: '//thememascot.us9.list-manage.com/subscribe/post?u=a01f440178e35febc8cf4e51f&amp;id=49d6d30e1e'
+                    });
+
+                    function mailChimpCallBack(resp) {
+                        // Hide any previous response text
+                        var $mailchimpform = $('#mailchimp-subscription-form-footer'),
+                            $response = '';
+                        $mailchimpform.children(".alert").remove();
+                        console.log(resp);
+                        if (resp.result === 'success') {
+                            $response = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + resp.msg + '</div>';
+                        } else if (resp.result === 'error') {
+                            $response = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + resp.msg + '</div>';
+                        }
+                        $mailchimpform.prepend($response);
+                    }
+                </script>
+            </div>
+            <div class="col-md-4 col-md-offset-3 mt-20">
+                <div class="pull-right">
+                    <ul class="styled-icons icon-bordered small square list-inline mt-10">
+                        <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter text-white"></i></a></li>
+                        <li><a href="#"><i class="fa fa-skype text-white"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube text-white"></i></a></li>
+                    </ul>
+                </div>
+                <div class="pull-left">
+                    <h5 class="text-white">Call Us Now</h5>
+                    <h4 class="text-gray">00144-636894</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid copy-right p-20 bg-black-333">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <p class="font-11 text-black-777 m-0">Copyright &copy;2015 ThemeMascot. All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script src="js/custom.js"></script>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,2 +1,147 @@
-<?php
-wp_head();
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="HealthZone - Daring Multi Concept Theme for Medical, Nursing, Yoga, Sports, Gym & Fitness" />
+    <meta name="keywords" content="building,business,construction,cleaning,transport,workshop" />
+    <meta name="author" content="ThemeMascot" />
+
+    <!-- Page Title -->
+    <title>HealthZone - Daring Multi Concept Theme for Medical, Nursing, Yoga, Sports, Gym & Fitness</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/css-plugin-collections.css" rel="stylesheet"/>
+    <!-- CSS | menuzord megamenu skins -->
+    <link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-boxed.css" rel="stylesheet"/>
+    <!-- CSS | Main style file -->
+    <link href="css/style-main.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Theme Color -->
+    <link href="css/colors/theme-skin-sky-blue.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="css/preloader.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+    <!-- external javascripts -->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link  href="js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+    <link  href="js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+    <link  href="js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- Revolution Slider 5.x EXTENSIONS  -->
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+
+<body class="">
+<div id="wrapper">
+    <!-- preloader -->
+    <div id="preloader">
+        <div id="spinner">
+            <img src="images/preloaders/1.gif" alt="">
+        </div>
+        <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+    </div>
+
+    <!-- Header -->
+    <header id="header" class="header">
+        <div class="header-top sm-text-center bg-theme-colored">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <nav>
+                            <ul class="list-inline sm-text-center text-left flip mt-5">
+                                <li> <a class="text-white" href="#">FAQ</a> </li>
+                                <li class="text-white">|</li>
+                                <li> <a class="text-white" href="#">Help Desk</a> </li>
+                                <li class="text-white">|</li>
+                                <li> <a class="text-white" href="#">Support</a> </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="widget m-0 mt-5 no-border">
+                            <ul class="list-inline text-right sm-text-center">
+                                <li class="pl-10 pr-10 mb-0 pb-0">
+                                    <div class="header-widget text-white"><i class="fa fa-phone"></i> 123-456-789 </div>
+                                </li>
+                                <li class="pl-10 pr-10 mb-0 pb-0">
+                                    <div class="header-widget text-white"><i class="fa fa-envelope-o"></i> contact@yourdomain.com </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-2 text-right flip sm-text-center">
+                        <div class="widget m-0">
+                            <ul class="styled-icons icon-dark icon-circled icon-theme-colored icon-sm">
+                                <li class="mb-0 pb-0"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li class="mb-0 pb-0"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li class="mb-0 pb-0"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li class="mb-0 pb-0"><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-nav">
+            <div class="header-nav-wrapper navbar-scrolltofixed bg-lighter">
+                <div class="container">
+                    <nav id="menuzord-right" class="menuzord orange no-bg"> <a class="menuzord-brand pull-left flip" href="javascript:void(0)"><img src="images/logo-wide.png" alt=""></a>
+                        <ul class="menuzord-menu onepage-nav">
+                            <li class="active"><a href="#home">Home</a></li>
+                            <li><a href="#departments">Departments</a></li>
+                            <li><a href="#doctors">Doctors</a></li>
+                            <li><a href="#pricing">Pricing</a></li>
+                            <li><a href="#gallery">Gallery</a></li>
+                            <li><a href="#blog">Blog</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
