161449caae15ceb08b2270f7e1939e036aaec7ab
tetrabirk <leyensl@isl-edu.be>
2017-11-09 19:00:59 +0100

    ok normalement tt est revenu a la normale

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         <div class="row border-bottom-black">
             <div class="col-sm-6 col-md-3">
                 <div class="widget dark">
-                    <img class="mt-10 mb-20" alt="" src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-wide-white.png">
+                    <img class="mt-10 mb-20" alt="" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo-wide-white.png">
                     <p>203, Envato Labs, Behind Alis Steet, Melbourne, Australia.</p>
                     <ul class="list-inline mt-5">
                         <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored mr-5"></i> <a class="text-gray" href="#">123-456-789</a> </li>
@@ -141,7 +141,7 @@
 
 <!-- Footer Scripts -->
 <!-- JS | Custom script for all pages -->
-<script src="<?php echo get_stylesheet_directory_uri() ?>/js/custom.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/custom.js"></script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -16,58 +16,58 @@
     <title>HealthZone - Daring Multi Concept Theme for Medical, Nursing, Yoga, Sports, Gym & Fitness</title>
 
     <!-- Favicon and Touch Icons -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.png" rel="shortcut icon" type="image/png">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/images/apple-touch-icon.png" rel="apple-touch-icon">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
 
     <!-- Stylesheet -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/jquery-ui.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/animate.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/css-plugin-collections.css" rel="stylesheet"/>
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/animate.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/css-plugin-collections.css" rel="stylesheet"/>
     <!-- CSS | menuzord megamenu skins -->
-    <link id="menuzord-menu-skins" href="<?php echo get_stylesheet_directory_uri() ?>/css/menuzord-skins/menuzord-boxed.css" rel="stylesheet"/>
+    <link id="menuzord-menu-skins" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/menuzord-skins/menuzord-boxed.css" rel="stylesheet"/>
     <!-- CSS | Main style file -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/style-main.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style-main.css" rel="stylesheet" type="text/css">
     <!-- CSS | Theme Color -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/colors/theme-skin-sky-blue.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/colors/theme-skin-sky-blue.css" rel="stylesheet" type="text/css">
     <!-- CSS | Preloader Styles -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/preloader.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/preloader.css" rel="stylesheet" type="text/css">
     <!-- CSS | Custom Margin Padding Collection -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
     <!-- CSS | Responsive media queries -->
-    <link href="<?php echo get_stylesheet_directory_uri() ?>/css/responsive.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/responsive.css" rel="stylesheet" type="text/css">
     <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
     <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
 
     <!-- external javascripts -->
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery-2.1.4.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery-ui.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/jquery-2.1.4.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/jquery-ui.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/bootstrap.min.js"></script>
     <!-- JS | jquery plugin collection for this theme -->
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery-plugin-collection.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/jquery-plugin-collection.js"></script>
 
     <!-- Revolution Slider 5.x CSS settings -->
-    <link  href="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
-    <link  href="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
-    <link  href="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+    <link  href="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+    <link  href="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+    <link  href="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
 
     <!-- Revolution Slider 5.x SCRIPTS -->
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
 
     <!-- Revolution Slider 5.x EXTENSIONS  -->
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -83,7 +83,7 @@
     <!-- preloader -->
     <div id="preloader">
         <div id="spinner">
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/preloaders/1.gif" alt="">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/preloaders/1.gif" alt="">
         </div>
         <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
     </div>
@@ -132,7 +132,7 @@
         <div class="header-nav">
             <div class="header-nav-wrapper navbar-scrolltofixed bg-lighter">
                 <div class="container">
-                    <nav id="menuzord-right" class="menuzord orange no-bg"> <a class="menuzord-brand pull-left flip" href="<?php echo get_stylesheet_directory_uri() ?>/javascript:void(0)"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-wide.png" alt=""></a>
+                    <nav id="menuzord-right" class="menuzord orange no-bg"> <a class="menuzord-brand pull-left flip" href="<?php echo get_stylesheet_directory_uri() ?>/assets/javascript:void(0)"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo-wide.png" alt=""></a>
                         <ul class="menuzord-menu onepage-nav">
                             <li class="active"><a href="#home">Home</a></li>
                             <li><a href="#departments">Departments</a></li>
